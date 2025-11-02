a2eb9cfb3cc0f6ed70973b8c26dbdabb01e97c26
toumaniaboubacarbamba <toumani.bamba@epitech.eu>
2025-11-02 15:45:47 +0000

    NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
+import About from "./components/About";
+import Experiences from "./components/Experiences";
+import Footer from "./components/Footer";
 import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
 
 export default function App() {
   return (
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,54 @@
+import { Container } from "lucide-react"
+
+const Navbar = () => {
+    return (
+        <div className="flex justify-center md:justify-between items-center p-4 ">
+            <a href="#"
+                className="flex items-center  font-bold  text-3xl md:text-xl"
+            >
+                <Container className="mr-2" />
+                Toumani
+                <span className="text-accent">DEV FULLSTACK</span>
+            </a>
+
+            <ul
+                className="hidden md:flex space-x-4">
+                <li>
+                    <a href="#Home"
+                        className="btn btn-sm btn-ghost"
+                    >
+                        Accueil
+                    </a>
+                </li>
+                <li>
+                    <a href="#About"
+                        className="btn btn-sm btn-ghost"
+                    >
+                        À propos
+                    </a>
+                </li>
+
+                <li>
+                    <a href="#Experiences"
+                        className="btn btn-sm btn-ghost"
+                    >
+                        Mes expériences
+                    </a>
+                </li>
+
+
+                <li>
+                    <a href="#Projects"
+                        className="btn btn-sm btn-ghost"
+                    >
+                        Mes projets
+                    </a>
+                </li>
+
+            </ul>
+
+        </div>
+    )
+}
+
+export default Navbar
